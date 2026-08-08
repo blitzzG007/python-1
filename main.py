cd7b83fed6c7ce7e4f7183c860283ccacbb971fd
Aly Kouyate <kouyategaly7@gmail.com>
2026-08-08 21:52:23 +0200

    initial update

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #Lets start my python journey - Lets goo
 
+#Veriable = A container for a value (string, integer, float, boolean)
+#           A veriable behaves as if it was the value it contains
+
+
+#Strings
 first_name = "Max"
 
 food = "pizza"
@@ -9,3 +14,9 @@ brand = "Nike"
 print(f"Character name: {first_name}")
 print(f"Favourite food: {food}")
 print(f"Favourite brand: {brand}")
+
+
+#Integers (numbers)
+age = 20
+
+print(f"You are {age} years old")
