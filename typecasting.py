c0f6fa14c7937dc464e32f91f368b8a8b3eb4845
Aly Kouyate <kouyategaly7@gmail.com>
2026-08-09 01:08:27 +0200

    Changed name of main.py to variables.py and added another file call typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+#Typecasting = the process of converting a variable from one data type to another
+#              str(), int(), float(), bool()
+
+
+name = "James Donut"
+age = 20
+gpa = 3.5
+is_student = True
+
---
+++ variables.py
@@ -0,0 +1,68 @@
+#Lets start my python journey - Lets goo
+
+#Variables = A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains
+
+
+# Strings
+first_name = "Max"
+
+food = "pizza"
+
+brand = "Nike"
+
+print(f"Character name: {first_name}")
+print(f"Favourite food: {food}")
+print(f"Favourite brand: {brand}")
+
+
+# Integers
+age = 27
+quantity = 3
+num_of_students = 32
+
+print(f"My age is {age}")
+print(f"You are buying {quantity} items")
+print(f"There are {num_of_students} students in this classrom")
+
+
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+
+# Boolean
+
+is_student = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student yet")
+
+car_for_sale = False
+other_cars_available = True
+
+if car_for_sale:
+    print("Yes, the car is for sale")
+elif other_cars_available:
+    print("No, this particular car is not for sale, but we have other cars.")
+else:
+    print("Sorry, we don't have any cars for sale")
+
+shadowking_is_online = True
+
+shadowking_is_offline = False
+
+if shadowking_is_online:
+    print("shadowking is online today")
+elif shadowking_is_offline:
+    print("I guess today he didn't come ge online today")
+else:
+    print("User shadowking doesn't exist anymore")
