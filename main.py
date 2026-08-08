bf7ca61ec3d91fef56e001b4945b6207317e23c7
Aly Kouyate <kouyategaly7@gmail.com>
2026-08-08 19:57:32 +0200

    Initial first commit on pyhton learning project

---
+++ main.py
@@ -0,0 +1,11 @@
+#Lets start my python journey - Lets goo
+
+first_name = "Max"
+
+food = "pizza"
+
+brand = "Nike"
+
+print(f"Character name: {first_name}")
+print(f"Favourite food: {food}")
+print(f"Favourite brand: {brand}")
