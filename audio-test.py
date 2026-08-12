313b54a0cfb61b2839f4a53db8c49f2c362e8cc7
Aly Kouyate <kouyategaly7@gmail.com>
2026-08-12 23:45:31 +0200

    modified the audio player

--- audio-test.py
+++ audio-test.py
@@ -1,8 +1,8 @@
 from gtts import gTTS
 
-text = "Bonjour Rahaf, je t’aime"
+text = "Messi and Ronaldo are two of the greatest footballers of all time, and their incredible careers, unique playing styles, countless goals, and unforgettable moments have made their rivalry one of the most famous stories in football history."
 
-tts = gTTS(text=text, lang="fr")
+tts = gTTS(text=text, lang="en")
 
 tts.save("voice.mp3")
 
