a56fddb7f0f0faec1560c33e723d4b65c617b29a
Aly Kouyate <kouyategaly7@gmail.com>
2026-08-13 00:25:14 +0200

    Modified to japanese language

--- audio-test.py
+++ audio-test.py
@@ -1,8 +1,8 @@
 from gtts import gTTS
 
-text = "Messi and Ronaldo are two of the greatest footballers of all time, and their incredible careers, unique playing styles, countless goals, and unforgettable moments have made their rivalry one of the most famous stories in football history."
+text = "メッシとロナウドは、史上最高のサッカー選手の二人であり、彼らの素晴らしいキャリア、独特なプレースタイル、数え切れないほどのゴール、そして忘れられない瞬間の数々によって、二人のライバル関係はサッカー史上最も有名な物語の一つとなっています。"
 
-tts = gTTS(text=text, lang="en")
+tts = gTTS(text=text, lang="ja")
 
 tts.save("voice.mp3")
 
